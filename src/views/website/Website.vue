eb2031519f25bcfc46aa07cbecb51e52b21b08bb
jinran01 <260043270@qq.com>
2024-04-11 17:43:35 +0800

    v1.2.0

--- src/views/website/Website.vue
+++ src/views/website/Website.vue
@@ -10,24 +10,24 @@
         </template>
         <div class="web_info">
           <el-form
-            label-position="right"
-            label-width="100px"
-            :model="webInfoConfig"
-            style="max-width: 460px"
+              label-position="right"
+              label-width="100px"
+              :model="webInfoConfig"
+              style="max-width: 460px"
           >
-            <el-form-item label="头 像">
-              <el-upload
-                action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"
-                :show-file-list="false"
-                :auto-upload="true"
-                :data="data"
-                :before-upload="before_upload"
-                :on-success="config_success"
-              >
-                <el-avatar shape="circle" :size="100" fit="fit" :src="webInfoConfig.websiteAvatar"/>
-              </el-upload>
-              <!--              <el-avatar shape="circle" :size="100" fit="fit" :src="userInfo.avatar" />-->
-            </el-form-item>
+            <!--            <el-form-item label="头 像">-->
+            <!--              <el-upload-->
+            <!--                action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"-->
+            <!--                :show-file-list="false"-->
+            <!--                :auto-upload="true"-->
+            <!--                :data="data"-->
+            <!--                :before-upload="before_upload"-->
+            <!--                :on-success="config_success"-->
+            <!--              >-->
+            <!--                <el-avatar shape="circle" :size="100" fit="fit" :src="webInfoConfig.websiteAvatar"/>-->
+            <!--              </el-upload>-->
+            <!--              &lt;!&ndash;              <el-avatar shape="circle" :size="100" fit="fit" :src="userInfo.avatar" />&ndash;&gt;-->
+            <!--            </el-form-item>-->
             <el-form-item label="网站名称">
               <el-input v-model="webInfoConfig.websiteName"/>
             </el-form-item>
@@ -42,8 +42,8 @@
             </el-form-item>
             <el-form-item label="网站创建日期">
               <el-date-picker
-                v-model="webInfoConfig.websiteCreateTime"
-                type="date"
+                  v-model="webInfoConfig.websiteCreateTime"
+                  type="date"
               />
             </el-form-item>
             <el-form-item label="备案号">
@@ -62,40 +62,40 @@
           </el-form>
         </div>
       </el-tab-pane>
+      <!--      <el-tab-pane name="second">-->
+      <!--        <template #label>-->
+      <!--          <span>-->
+      <!--            <el-icon><User/></el-icon>-->
+      <!--            <span>社交信息</span>-->
+      <!--          </span>-->
+      <!--        </template>-->
+      <!--        <el-form-->
+      <!--          label-position="right"-->
+      <!--          label-width="100px"-->
+      <!--          :model="webInfoConfig"-->
+      <!--          style="max-width: 460px"-->
+      <!--        >-->
+      <!--          <el-checkbox-group v-model="webInfoConfig.socialUrlList">-->
+      <!--            <el-form-item label="QQ">-->
+      <!--              <el-input v-model="webInfoConfig.qq"/>-->
+      <!--              <el-checkbox label="qq">是否展示</el-checkbox>-->
+      <!--            </el-form-item>-->
+      <!--            <el-form-item label="Github">-->
+      <!--              <el-input v-model="webInfoConfig.github"/>-->
+      <!--              <el-checkbox label="github">是否展示</el-checkbox>-->
+      <!--            </el-form-item>-->
+      <!--            <el-form-item label="Gitee">-->
+      <!--              <el-input v-model="webInfoConfig.gitee"/>-->
+      <!--              <el-checkbox label="gitee">是否展示</el-checkbox>-->
+      <!--            </el-form-item>-->
+      <!--          </el-checkbox-group>-->
+      <!--          <el-form-item>-->
+      <!--            <el-button style="margin: 20px 20px;width: 100px" type="primary" @click="saveWebInfoConfig">保 存-->
+      <!--            </el-button>-->
+      <!--          </el-form-item>-->
+      <!--        </el-form>-->
+      <!--      </el-tab-pane>-->
       <el-tab-pane name="second">
-        <template #label>
-          <span>
-            <el-icon><User/></el-icon>
-            <span>社交信息</span>
-          </span>
-        </template>
-        <el-form
-          label-position="right"
-          label-width="100px"
-          :model="webInfoConfig"
-          style="max-width: 460px"
-        >
-          <el-checkbox-group v-model="webInfoConfig.socialUrlList">
-            <el-form-item label="QQ">
-              <el-input v-model="webInfoConfig.qq"/>
-              <el-checkbox label="qq">是否展示</el-checkbox>
-            </el-form-item>
-            <el-form-item label="Github">
-              <el-input v-model="webInfoConfig.github"/>
-              <el-checkbox label="github">是否展示</el-checkbox>
-            </el-form-item>
-            <el-form-item label="Gitee">
-              <el-input v-model="webInfoConfig.gitee"/>
-              <el-checkbox label="gitee">是否展示</el-checkbox>
-            </el-form-item>
-          </el-checkbox-group>
-          <el-form-item>
-            <el-button style="margin: 20px 20px;width: 100px" type="primary" @click="saveWebInfoConfig">保 存
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </el-tab-pane>
-      <el-tab-pane name="third">
         <template #label>
           <span>
             <el-icon><Setting/></el-icon>
@@ -103,44 +103,44 @@
           </span>
         </template>
         <el-form
-          label-position="right"
-          label-width="100px"
-          :model="webInfoConfig"
-          style="max-width: 460px"
+            label-position="right"
+            label-width="100px"
+            :model="webInfoConfig"
+            style="max-width: 460px"
         >
           <el-form-item label="用户头像">
             <el-upload
-              action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"
-              :show-file-list="false"
-              :auto-upload="true"
-              :data="data"
-              :before-upload="before_upload"
-              :on-success="userAvatar"
+                :action="action"
+                :show-file-list="false"
+                :auto-upload="true"
+                :data="data"
+                :before-upload="before_upload"
+                :on-success="userAvatar"
             >
-              <el-avatar shape="circle" :size="100" fit="fit" :src="webInfoConfig.websiteAvatar"/>
+              <el-avatar shape="circle" :size="100" fit="fit" :src="webInfoConfig.userAvatar"/>
             </el-upload>
           </el-form-item>
           <el-form-item label="游客头像">
             <el-upload
-              action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"
-              :show-file-list="false"
-              :auto-upload="true"
-              :data="data"
-              :before-upload="before_upload"
-              :on-success="visitor_success"
+                :action="action"
+                :show-file-list="false"
+                :auto-upload="true"
+                :data="data"
+                :before-upload="before_upload"
+                :on-success="visitor_success"
             >
-              <el-avatar shape="circle" :size="100" fit="fit" :src="webInfoConfig.websiteAvatar"/>
+              <el-avatar shape="circle" :size="100" fit="fit" :src="webInfoConfig.touristAvatar"/>
             </el-upload>
           </el-form-item>
           <el-form-item label="默认文章封面">
             <el-upload
-              action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"
-              :show-file-list="false"
-              :auto-upload="true"
-              :data="data"
-              :before-upload="before_upload"
-              :on-success="cover_success"
-              class="el-upload_img"
+                :action="action"
+                :show-file-list="false"
+                :auto-upload="true"
+                :data="data"
+                :before-upload="before_upload"
+                :on-success="cover_success"
+                class="el-upload_img"
             >
               <el-image :src="webInfoConfig.articleCover" style="border-radius: 10px"></el-image>
             </el-upload>
@@ -163,44 +163,47 @@
               <el-radio :label=0 size="large">关闭</el-radio>
             </el-radio-group>
           </el-form-item>
-          <el-form-item label="打赏状态">
-            <el-radio-group v-model="webInfoConfig.isReward">
-              <el-radio :label=1 size="large">开启</el-radio>
-              <el-radio :label=0 size="large">关闭</el-radio>
-            </el-radio-group>
-          </el-form-item>
+          <!--          <el-form-item label="打赏状态">-->
+          <!--            <el-radio-group v-model="webInfoConfig.isReward">-->
+          <!--              <el-radio :label=1 size="large">开启</el-radio>-->
+          <!--              <el-radio :label=0 size="large">关闭</el-radio>-->
+          <!--            </el-radio-group>-->
+          <!--          </el-form-item>-->
           <!--打赏二维码-->
-          <div v-if="webInfoConfig.isReward == 1">
-            <el-form-item label="微信二维码">
-              <el-upload
-                action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"
-                :show-file-list="false"
-                :auto-upload="true"
-                :data="data"
-                :before-upload="before_upload"
-                :on-success="wechat_success"
-              >
-                <el-avatar shape="square" :size="100" fit="fit" :src="webInfoConfig.weiXinQRCode"/>
-              </el-upload>
-            </el-form-item>
-            <el-form-item label="支付宝二维码">
-              <el-upload
-                action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"
-                :show-file-list="false"
-                :auto-upload="true"
-                :data="data"
-                :before-upload="before_upload"
-                :on-success="alipay_success"
-              >
-                <el-avatar shape="square" :size="100" fit="fit" :src="webInfoConfig.alipayQRCode"/>
-              </el-upload>
-            </el-form-item>
-          </div>
+          <!--          <div v-if="webInfoConfig.isReward == 1">-->
+          <!--            <el-form-item label="微信二维码">-->
+          <!--              <el-upload-->
+          <!--                  :action=action-->
+          <!--                  :show-file-list="false"-->
+          <!--                  :auto-upload="true"-->
+          <!--                  :data="data"-->
+          <!--                  :before-upload="before_upload"-->
+          <!--                  :on-success="wechat_success"-->
+          <!--              >-->
+          <!--                <el-avatar shape="square" :size="100" fit="fit" :src="webInfoConfig.weiXinQRCode"/>-->
+          <!--              </el-upload>-->
+          <!--            </el-form-item>-->
+          <!--            <el-form-item label="支付宝二维码">-->
+          <!--              <el-upload-->
+          <!--                  action="https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"-->
+          <!--                  :show-file-list="false"-->
+          <!--                  :auto-upload="true"-->
+          <!--                  :data="data"-->
+          <!--                  :before-upload="before_upload"-->
+          <!--                  :on-success="alipay_success"-->
+          <!--              >-->
+          <!--                <el-avatar shape="square" :size="100" fit="fit" :src="webInfoConfig.alipayQRCode"/>-->
+          <!--              </el-upload>-->
+          <!--            </el-form-item>-->
+          <!--          </div>-->
           <el-form-item label="聊天室状态">
             <el-radio-group v-model="webInfoConfig.isChatRoom">
               <el-radio :label=1 size="large">开启</el-radio>
               <el-radio :label=0 size="large">关闭</el-radio>
             </el-radio-group>
+          </el-form-item>
+          <el-form-item label="Websocket" v-if="webInfoConfig.isChatRoom">
+            <el-input v-model="webInfoConfig.websocketUrl"></el-input>
           </el-form-item>
           <el-form-item label="播放器状态">
             <el-radio-group v-model="webInfoConfig.isMusicPlayer">
@@ -223,6 +226,7 @@ import {getConfigOssToken, getWebConfig, updateWebConfig} from "@/network/webcon
 import {onMounted, reactive, ref} from "vue";
 import {ElMessage} from "element-plus";
 import {v4} from "uuid";
+import {getPolicy} from "@/network/system";
 
 export default {
   name: "Website",
@@ -235,6 +239,7 @@ export default {
       'OSSAccessKeyId': '',
       'signature': '',
     };
+    let action = ref("https://legaladvice.oss-cn-beijing.aliyuncs.com/");
     let webInfoConfig = ref({
       //网站基本设置
       websiteAvatar: '', //网站头像
@@ -283,41 +288,45 @@ export default {
         flag = false
       }
       return new Promise((resolve, reject) => {
-        getConfigOssToken().then(res => {
-          data.signature = res.data.signature,
-            data.policy = res.data.policy,
-            data.OSSAccessKeyId = res.data.accessKeyId,
-            data.key = res.data.dir + v4() + "." + type,
-            resolve(flag)
+        getPolicy({path:"default"}).then(res => {
+          data.signature = res.data.signature
+          data.policy = res.data.policy
+          data.OSSAccessKeyId = res.data.accessKeyId
+          data.key = res.data.dir + v4() + "." + type
+          setTimeout(() => {
+            resolve(file)
+          }, 2000)
         })
       })
     }
-    let url = "https://spring-boot-blog-api.oss-cn-hangzhou.aliyuncs.com/"
+    let url = action.value
     //网站头像上传成功回调
     const config_success = () => {
-      webInfoConfig.value.websiteAvatar = url+data.key
+      webInfoConfig.value.websiteAvatar = url + data.key
     }
     //用户默认头像上传成功回调
     const userAvatar = () => {
-      webInfoConfig.value.userAvatar = url+data.key
+      webInfoConfig.value.userAvatar = url + data.key
+      saveWebInfoConfig()
     }
     //游客默认头像修改成功回调
     const visitor_success = () => {
-      webInfoConfig.value.touristAvatar = url+data.key
+      webInfoConfig.value.touristAvatar = url + data.key
+      saveWebInfoConfig()
     }
     //文章默认封面上传成功回调
     const cover_success = () => {
-      webInfoConfig.value.articleCover = url+data.key
-      console.log(webInfoConfig.value.articleCover)
+      webInfoConfig.value.articleCover = url + data.key
+      saveWebInfoConfig()
     }
     //微信打赏二维码上传成功回调
-    const wechat_success = () => {
-      webInfoConfig.value.weiXinQRCode = url+data.key
-    }
+    // const wechat_success = () => {
+    //   webInfoConfig.value.weiXinQRCode = url + data.key
+    // }
     //支付宝打赏二维码上传成功回调
-    const alipay_success = () => {
-      webInfoConfig.value.alipayQRCode = url+data.key
-    }
+    // const alipay_success = () => {
+    //   webInfoConfig.value.alipayQRCode = url + data.key
+    // }
     //上传成功回调
     // const success = (data) => {
     //   console.log(data)
@@ -373,13 +382,14 @@ export default {
       otherConfig,
       activeName,
       data,
+      action,
       before_upload,
       config_success,
       cover_success,
       visitor_success,
       userAvatar,
-      wechat_success,
-      alipay_success,
+      // wechat_success,
+      // alipay_success,
       saveWebInfoConfig,
     }
   }
@@ -403,6 +413,7 @@ export default {
   padding: 20px;
   border: 1px solid #ebeef5;
   border-radius: 5px;
+
   i {
     margin-right: 5px;
   }
